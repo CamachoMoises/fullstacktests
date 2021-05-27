05da416934ebc6c8aa35f665b382fac473b74e29
Moises Camacho <moisescamacho26@gmail.com>
2021-05-27 11:45:50 -0400

    Adding CORS and put method for toggle importance of notes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 let notes = [
   {
     id: 1,
@@ -21,41 +22,45 @@ let notes = [
   },
 ];
 app.use(express.json());
+app.use(cors());
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
 
-app.use(requestLogger)
+app.use(requestLogger);
 
 const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
-  }
-
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
 
 app.post("/api/notes", (request, response) => {
   const body = request.body;
-  if(!body.content){
-      return response.status(400).json({error:'content missing'})
+  if (!body.content) {
+    return response.status(400).json({ error: "content missing" });
   }
-  const note={
-      content:body.content,
-      important:body.important || false,
-      date:new Date(),
-      id:generateId()
-  }
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
   notes = notes.concat(note);
   response.json(note);
 });
 
 app.get("/api/notes", (request, response) => {
   response.json(notes);
+});
+app.put("/api/notes/edit/:id", (request, response) => {
+  const body = request.body;
+  const id = request.params.id;
+  notes=notes.map(note=>note!==id?note:body)
+  response.json(body)
 });
 
 app.get("/", (request, response) => {
@@ -80,11 +85,10 @@ app.delete("/api/notes/:id", (request, response) => {
 });
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-app.use(unknownEndpoint)
-
+app.use(unknownEndpoint);
 
 // const app = http.createServer((request, response) => {
 //   response.writeHead(200, { "Content-Type": "application/json" });
