5545bed8968577976f72afd5a612d3e6e522f6c4
Moises Camacho <moisescamacho26@gmail.com>
2021-05-27 15:22:27 -0400

    adding file for Heroku and port in env file

--- index.js
+++ index.js
@@ -94,6 +94,6 @@ app.use(unknownEndpoint);
 //   response.writeHead(200, { "Content-Type": "application/json" });
 //   response.end(JSON.stringify(notes));
 // });
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
